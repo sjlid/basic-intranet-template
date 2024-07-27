4388466547889efd8eba1532e3818aa98345def9
sjlid <senjohn.0000@gmail.com>
2024-07-27 20:40:50 +0300

    wip flyway

---
+++ src/main/resources/db/migration/V2__create-user-admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (
+    id, department, email, image_name, job_title, login,
+    "password", phone, "role", user_name, user_surname
+)
+VALUES (1, 'admin_department', 'admin_email', null, 'admin_title', 'admin', '749f09bade8aca755660eeb17792da880218d4fbdc4e25fbec279d7fe9f65d70', 'admin_phone', 'ROLE_ADMIN', 'admin_name', 'admin_surname')
