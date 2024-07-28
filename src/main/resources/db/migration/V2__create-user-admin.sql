824428a08e54b3e99888b4e70a4a2bd57117c9a0
sjlid <senjohn.0000@gmail.com>
2024-07-28 22:14:08 +0300

    add correct password for admin role in flyway script

--- src/main/resources/db/migration/V2__create-user-admin.sql
+++ src/main/resources/db/migration/V2__create-user-admin.sql
@@ -2,4 +2,4 @@ INSERT INTO users (
     id, department, email, image_name, job_title, login,
     "password", phone, "role", user_name, user_surname
 )
-VALUES (1, 'admin_department', 'admin_email', null, 'admin_title', 'admin', '749f09bade8aca755660eeb17792da880218d4fbdc4e25fbec279d7fe9f65d70', 'admin_phone', 'ROLE_ADMIN', 'admin_name', 'admin_surname')
+VALUES (1, 'admin_department', 'admin_email', null, 'admin_title', 'admin', '$2a$10$Q5Frrcn8uhPVeIRd7KuRye6ZV9zyUVmaGpJCuYBdUihmY6yVgbLuG', 'admin_phone', 'ROLE_ADMIN', 'admin_name', 'admin_surname')
